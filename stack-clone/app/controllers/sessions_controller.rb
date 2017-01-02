815400cb4f924cc288ddef7e050e754f997696b9
Tyler Small <email": null>
2017-01-02 15:54:44 -0600

    Add login support for users

--- stack-clone/app/controllers/sessions_controller.rb
+++ stack-clone/app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:session][:username])
-    if user && user.authenticate(params[:session])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
       redirect_to root_path
     else
       @errors = ['Invalid Username/Password Combination']
