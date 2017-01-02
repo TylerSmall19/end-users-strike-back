40c4815841c0440da4932b3e216c04ffbdd0c7cc
Tyler Small <email": null>
2017-01-02 16:20:27 -0600

    Add support for profile linking. Add partial logout support

--- stack-clone/config/routes.rb
+++ stack-clone/config/routes.rb
@@ -1,17 +1,18 @@
 Rails.application.routes.draw do
-  resources :questions
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'questions#index'
 
+  resources :questions
+
   get '/login'  => 'sessions#new'
   post '/login' => 'sessions#create'
 
   delete '/logout' => 'sessions#destroy'
 
+  get '/users/:id' => 'users#show', as: :user
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
