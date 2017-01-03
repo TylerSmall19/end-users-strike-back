9699243cb0f4b266d40bfd56c9890f728ea637f9
Tyler Small <email": null>
2017-01-02 19:11:22 -0600

    Add support for answering a question

--- stack-clone/app/controllers/answers_controller.rb
+++ stack-clone/app/controllers/answers_controller.rb
@@ -2,10 +2,10 @@ class AnswersController < ApplicationController
   def create
     @question = Question.find_by(id: params[:id])
     if logged_in?
-      answer = @question.answers.create(answer_params)
+      answer = @question.answers.new(answer_params)
       answer.author = current_user
       if answer.save
-
+        render '/questions/show'
       else
         @errors = answer.errors.full_messages
         render '/questions/show'
